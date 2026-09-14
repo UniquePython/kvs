2edd61ce2863e42d1d3c3fde9f02e2a44dad4056
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-09-14 10:01:34 +0530

    code: Pin down type kind backing values

--- include/kvs/typekind.h
+++ include/kvs/typekind.h
@@ -1,13 +1,15 @@
 #ifndef KVS_TYPEKIND_H_
 #define KVS_TYPEKIND_H_
 
+// clang-format off
 typedef enum
 {
-    TK_U8,
-    TK_U16,
-    TK_U32,
-    TK_U64,
+    TK_U8  = 1,
+    TK_U16 = 2,
+    TK_U32 = 3,
+    TK_U64 = 4,
 
 } TypeKind;
+// clang-format on
 
 #endif
